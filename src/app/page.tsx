6827af92d4aa295323262d05fbb7e869881460f6
Phluynhh <22520778@gm.uit.edu.vn>
2026-03-16 10:53:30 +0700

    feat: add custom scrollbar styles and improve layout structure in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import About from "@/components/ui/About";
 import Header from "@/components/ui/Header";
 import Overall from "@/components/ui/Overall";
 
 export default function Home() {
   return (
-    <div className="w-full">
+    <div className="w-full flex-col">
       <Header />
       <div className="mt-20">
         <Overall />
+      </div>
+      <div className="mt-4">
+        <About />
       </div>
     </div>
   );
---
+++ src/components/ui/About.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="relative overflow-hidden">
+      <div className="mx-auto flex w-full max-w-6xl flex-col items-center justify-between gap-12 py-8 lg:flex-row">
+        <p>hihi</p>
+      </div>
+    </section>
+  );
+}
